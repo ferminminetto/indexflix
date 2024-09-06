7a312978d9a39fe62d5a803c2541ce642f8ebcf6
Fermin Minetto <fermin.minetto.tech@gmail.com>
2024-09-06 19:16:09 -0300

    Added Search Movies Functionality

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -1,11 +1,16 @@
+import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import patch
 from app.main import app
 from tests.mock_data import MOCK_MOVIES
 from app.routers.movies import index_movies
+from app.utils.elastic import get_es_client
 
 
 client = TestClient(app)
+es = get_es_client()
+
+INDEX_NAME = "movies_test"
 
 @patch("app.routers.movies.get_es_client")
 @patch("app.routers.movies.requests.get")
@@ -60,3 +65,65 @@ def test_index_movies_unexpected_error(mock_get, mock_es_client, caplog):
 
     assert response.status_code == 500
     assert response.json() == {"detail": "An unexpected error occurred"}
+
+def seed_elasticsearch():
+    mapping = {
+        "mappings": {
+            "properties": {
+                "Title": {"type": "text"},
+                "Year": {"type": "integer"}
+            }
+        }
+    }
+
+    es.indices.delete(index=INDEX_NAME, ignore=[400, 404])
+    es.indices.create(index=INDEX_NAME, body=mapping)
+
+    for movie in MOCK_MOVIES["data"]:
+        es.index(index=INDEX_NAME, document=movie)
+
+    # This ensures data is available for search after seeding. "indexing lag"
+    es.indices.refresh(index=INDEX_NAME)  
+
+def clean_elasticsearch():
+    es.indices.delete(index=INDEX_NAME, ignore=[400, 404])
+
+@pytest.fixture(scope="function", autouse=True)
+def setup_and_teardown():
+    seed_elasticsearch()
+    yield
+    clean_elasticsearch()
+
+@patch("app.routers.movies.INDEX_NAME", new=INDEX_NAME)
+def test_search_by_title():
+    response = client.get("/movies/search/?title=antman")
+    assert response.status_code == 200
+    movies = response.json()
+    assert len(movies) == 1
+    assert movies[0]["Title"] == "The Antman"
+
+@patch("app.routers.movies.INDEX_NAME", new=INDEX_NAME)
+def test_search_by_year():
+    response = client.get("/movies/search/?year=2015")
+    assert response.status_code == 200
+    movies = response.json()
+    assert len(movies) == 2
+    assert movies[0]["Title"] == "Maze Runner: The Scorch Trials"
+
+@patch("app.routers.movies.INDEX_NAME", new=INDEX_NAME)
+def test_search_by_title_and_year():
+    response = client.get("/movies/search/?title=Maze Runner&year=2015")
+    assert response.status_code == 200
+    movies = response.json()
+    breakpoint()
+    
+    # This time retrieved the only correct option.
+    assert len(movies) == 1
+    assert movies[0]["Title"] == "Maze Runner: The Scorch Trials"
+
+@patch("app.routers.movies.INDEX_NAME", new=INDEX_NAME)
+def test_no_movies_found():
+    response = client.get("/movies/search/?title=NonExistentMovie")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No movies found"}
+
